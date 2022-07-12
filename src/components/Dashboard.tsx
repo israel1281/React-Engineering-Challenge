1e256dbf7da76d0d8b24c99f6928779a3d97773a
Israel David <77063876+israel1281@users.noreply.github.com>
2022-07-12 14:37:56 -0700

    transaction content designed completed

--- src/components/Dashboard.tsx
+++ src/components/Dashboard.tsx
@@ -14,6 +14,12 @@ interface Props {
     input: string;
     filters: string;
     filtersContainer: string;
+    transactionDetails: string;
+    transactionDate: string;
+    transactionsContent: string;
+    icon: string;
+    iconIcon: string;
+    details1: string;
   };
 }
 export const Dashboard: FC<Props> = memo(function IPhone11ProX1(props = {}) {
@@ -39,6 +45,67 @@ export const Dashboard: FC<Props> = memo(function IPhone11ProX1(props = {}) {
         <button>Withdrawal</button>
         <button>Deposit</button>
         <button>Loan</button>
+      </div>
+      <div
+        className={`${classes.transactionDetails} ${
+          props.classes?.transactionDetails || ""
+        }`}
+      >
+        <span
+          className={`${classes.transactionDate} ${
+            props.classes?.transactionDate || ""
+          }`}
+        >
+          10th July, 2022
+        </span>
+        <div
+          className={`${classes.transactionsContent} ${
+            props.classes?.transactionsContent || ""
+          }`}
+        >
+          <div className={`${classes.icon} ${props.classes?.icon || ""}`}>
+            <WithdrawalIcon
+              className={`${classes.iconIcon} ${props.classes?.iconIcon || ""}`}
+            />
+          </div>
+          <div
+            className={`${classes.details1} ${props.classes?.details1 || ""}`}
+          >
+            <span
+              style={{
+                color: "#000",
+                fontSize: "16px",
+                fontWeight: "bold",
+                textTransform: "uppercase",
+                fontFamily: "Quicksand"
+              }}
+            >
+              Susan Doe
+            </span>
+            <span
+              style={{
+                color: "#000",
+                fontSize: "12px",
+                textTransform: "uppercase",
+                fontFamily: "Quicksand"
+              }}
+            >
+              07:15 PM
+            </span>
+          </div>
+          <span
+            style={{
+              color: "#d73d3d",
+              fontSize: "16px",
+              fontWeight: "bold",
+              fontFamily: "Quicksand",
+              textTransform: "uppercase",
+              margin: "auto 0px"
+            }}
+          >
+            -5,000.00
+          </span>
+        </div>
       </div>
     </div>
   );
