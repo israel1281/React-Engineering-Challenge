45592ec6abcc8e05fce53abb0a92b109764088ec
Israel David <77063876+israel1281@users.noreply.github.com>
2022-07-12 12:23:01 -0700

    icons folder

--- src/App.tsx
+++ src/App.tsx
@@ -1,4 +1,5 @@
 import { FC, memo } from "react";
+import { SearchIcon } from "./components/icons/SearchIcon";
 
 import classes from "./App.module.css";
 
@@ -6,5 +7,9 @@ interface Props {
   className?: string;
 }
 export const App: FC<Props> = memo(function App(props = {}) {
-  return <div className={`${classes.root} ${props.className || ""}`}></div>;
+  return (
+    <div className={`${classes.root} ${props.className || ""}`}>
+      <SearchIcon />
+    </div>
+  );
 });
