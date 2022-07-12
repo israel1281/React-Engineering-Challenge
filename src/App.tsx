e0d3371826c718437068a28314e13876bb651da4
Israel David <77063876+israel1281@users.noreply.github.com>
2022-07-12 12:19:07 -0700

    folder structure

--- src/App.tsx
+++ src/App.tsx
@@ -1,15 +1,10 @@
-import { FC, memo } from 'react';
+import { FC, memo } from "react";
 
-import classes from './App.module.css';
-import { IPhone11ProX1 } from './components/IPhone11ProX1';
+import classes from "./App.module.css";
 
 interface Props {
   className?: string;
 }
 export const App: FC<Props> = memo(function App(props = {}) {
-  return (
-    <div className={`${classes.root} ${props.className || ''}`}>
-      <IPhone11ProX1 />
-    </div>
-  );
+  return <div className={`${classes.root} ${props.className || ""}`}></div>;
 });
